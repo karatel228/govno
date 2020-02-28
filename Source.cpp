0a1fc1f773d8c8e2835c40b63704761d44122442
Sashka <sasha_vola@mail.ru>
2020-02-28 12:14:29 +0100

    last last

--- Source.cpp
+++ Source.cpp
@@ -3,20 +3,33 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <map>
+#include <set>
+#include <array>
+#include <algorithm> 
 
 
 using namespace std;
 
 struct Node {
 	string name;
+	int program_point [2] = {0, 0};
+	int label;
 	Node* parent;
 	Node* next;
 	vector<Node*> children;
 };
 
+struct Pattern {
+	string name;
+	string label;
+	vector<Pattern*> patterns;
+};
+
 Node* init(string a)
 {
 	struct Node* lst = new Node;
+	lst->label = 0;
 	lst->parent = 0;
 	lst->next = 0;
 	lst->name = a;
@@ -51,6 +64,17 @@ vector<string> split(string str, const string& delimiter) {
 	}
 
 	return tokens;
+}
+int ZhangSasha(Node* root1, Node* root2) {
+	return 5;
+}
+
+vector<int> keyrots(Node* root) {
+	vector<int> keys;
+	keys.push_back(1);
+	keys.push_back(2);
+
+	return keys;
 }
 
 vector<vector<string>> split_rule(string str) {
@@ -593,23 +617,110 @@ int LevenshteinDistance(vector<string> &source,
 	return lev_dist[min_size];
 }
 
+vector<int> keyroots(Node* root) {
+	vector<int> keyroots;
+	for (int i = 0; i < root->children.size(); i++) {
+		int flag = 0;
+		for (int j = i + 1; j < root->children.size(); j++) {
+			if (root->children[j] == root->children[i]) {
+				flag = 1;
+			}
+		}
+		if (flag == 0) {
+			keyroots.push_back(i + 1);
+		}
+	}
+
+	return keyroots;
+}
+
+int** treedist(Node* l1, Node* l2, int i, int j, Node* root1, Node* root2) {
+
+	int** forestdist = new int* [i + 1];
+	for (int i = 0; i < i + 1; i++)
+		forestdist[i] = new int[j + 1];
+
+	int Delete = 1;
+	int Insert = 1;
+	int Relabel = 1;
+
+	forestdist[0][0] = 0;
+	for (int i1 = l1->children.size(); i1 <= i; i1++) {
+		forestdist[i1][0] = forestdist[i1 - 1][0] + Delete;
+	}
+	for (int j1 = l2->children.size(); j1 <= j; j1++) {
+		forestdist[0][j1] = forestdist[0][j1 - 1] + Insert;
+	}
+	for (int i1 = l1->children.size(); i1 <= i; i1++) {
+		for (int j1 = l2->children.size(); j1 <= j; j1++) {
+			int i_temp = (l1->children.size() > i1 - 1) ? 0 : i1 - 1;
+			int j_temp = (l2->children.size() > j1 - 1) ? 0 : j1 - 1;
+			if ((l1->children.size() == l1->children.size()) && (l2->children.size() == l2->children.size())) {
+
+				int Cost = (root1->name == root2->name) ? 0 : Relabel;
+				forestdist[i1][j1] = min(
+					min(forestdist[i_temp][j1] + Delete, forestdist[i1][j_temp] + Insert),
+					forestdist[i_temp][j_temp] + Cost);
+			}
+			else {
+				int i1_temp = l1->children.size() - 1;
+				int j1_temp = l2->children.size() - 1;
+
+				int i_temp2 = (l1->children.size() > i1_temp) ? 0 : i1_temp;
+				int j_temp2 = (l2->children.size() > j1_temp) ? 0 : j1_temp;
+
+				forestdist[i1][j1] = min(
+					min(forestdist[i_temp][j1] + Delete, forestdist[i1][j_temp] + Insert),
+					forestdist[i_temp2][j_temp2]);
+			}
+		}
+	}
+	return forestdist;
+}
+
+
+int** ZhangShasha(Node* root1, Node* root2) {
+	Node* l1 = root1->children.front();
+	vector<int> keyroots1 = keyroots(root1);
+	Node* l2 = root2->children.front();
+	vector<int> keyroots2 = keyroots(root2);
+
+	// space complexity of the algorithm
+	int** TD = new int* [l1->children.size() + 1];
+	for ( int i = 0; i < l1->children.size() + 1; i++)
+		TD[i] = new int[l2->children.size() + 1];
+
+	// solve subproblems
+	for (int i1 = 1; i1 < keyroots1.size() + 1; i1++) {
+		for (int j1 = 1; j1 < keyroots2.size() + 1; j1++) {
+			int i = keyroots1[i1 - 1];
+			int j = keyroots2[j1 - 1];
+			TD = treedist(l1, l2, i, j, root1, root2);
+		}
+	}
+
+	return TD;
+}
+
+
+
 vector<vector<string>> print_tree(Node* initial) {
 	
 		vector <vector<string>> output;
 		output.push_back(vector<string>());
-		output.back().push_back(initial->name);
+		output.back().push_back(initial->name + ";=" + to_string(initial->label) + "=" + to_string(initial->program_point[0]) + ";" + to_string(initial->program_point[1]));
 		output.push_back(vector<string>());
 		struct Node* current = new Node;
 		current = initial->children[0];
+		int pos = 0;
 		int line = 0;
 		int length_max = 0;
 	
 		while (1) {
-			int pos = 0;
 			line++;
 			int length = 0;
 			int length_cur = 0;
-			output[line].push_back(current->name);
+			output[line].push_back(current->name+";="+ to_string(current->label)+"=" +to_string(current->program_point[0])+";"+ to_string(current->program_point[1]));
 			output[line].push_back(" ");
 			for (auto& elem : output[line]) {
 				length_cur += elem.length();
@@ -627,7 +738,7 @@ vector<vector<string>> print_tree(Node* initial) {
 					} 
 				}
 			}
-			if (current->name.front() == '<' && current->name.size()==1 || current->name.front() != '<') {
+			if (current->children.empty()) {
 				int start = line;
 				if (current->parent->children.size() > 1) {
 					start = line + 1;
@@ -647,15 +758,28 @@ vector<vector<string>> print_tree(Node* initial) {
 				while (1) {
 					if (current->next != 0) {
 						current = current->next;
+						pos++;
 						line = line--;
 						break;
 					}
 					else {
-						current = current->parent;
-						if (current == initial)
-							return output;
-						line--;
-						continue;
+						if (current != current->parent->children.back()) {
+							for (int i = 0; i < current->parent->children.size(); i++) {
+								if (current->parent->children[i] == current) {
+									current = current->parent->children[i+1];
+									line--;
+									break;
+								}
+							}	
+							break;
+						}
+						else {
+							current = current->parent;
+							if (current == initial)
+								return output;
+							line--;
+							continue;
+						}					
 					}
 				}
 			}
@@ -679,6 +803,49 @@ vector<vector<string>> print_tree(Node* initial) {
 		delete current;
 				
 	}
+
+Pattern* anty_unity(Node* root1, Node* root2,set<string>& worktable) {
+
+	
+	Pattern* pattern = new Pattern();
+	pattern->label = to_string(root1->label) + ";"+ to_string(root2->label) + ";";
+	set<string>::iterator it = worktable.find( to_string(root1->label) + ";"  + to_string(root2->label) + ";");
+	if (it == worktable.end()) {
+
+		worktable.insert(to_string(root1->label) + ";" + to_string(root2->label) + ";");
+	}
+
+	if (root1->name != root2->name || root1->name == root2->name && root1->children.size() != root2->children.size()) {
+		if (keyrots(root1).size() > 3 && keyrots(root2).size() >3) {
+			int dist = ZhangSasha(root1, root2);
+			int min_keyroots = (keyroots(root1).size() > keyroots(root2).size()) ? keyroots(root2).size() : keyroots(root1).size();
+			if (dist < min_keyroots/2)
+				pattern->name = "yes";
+			else
+				pattern->name = "no";
+		}
+		else
+			pattern->name = "yes";
+		return pattern;
+	}
+	else {
+		if (root1->children.size() == root2->children.size() && root1->children.size() == 0) {
+			pattern->name = root1->name;
+			return pattern;
+		}
+		else {
+			pattern->name = root1->name;
+			int threshold = (root1->children.size() >= root2->children.size()) ? root2->children.size() : root1->children.size();
+			for (int i = 0; i < threshold; i++) {
+				Pattern* pattern_child = anty_unity(root1->children[i], root2->children[i], worktable);
+				pattern->patterns.push_back(pattern_child);
+			}
+			return pattern;
+
+		}
+	}
+	
+}
 
 //vector<tree*> build_table(vector<vector<string>>& grammar) {
 //	vector<tree*> syntax_tree;
@@ -896,9 +1063,9 @@ int main() {
 		fout << "==============================================================================================================================" << endl;
 
 		path.clear();
-		vector<string> inputs;
+		vector<vector<string>> inputs;
 		for (int i = 1; i <= 2; i++) {
-			inputs.push_back("");
+			inputs.push_back(vector<string>());
 			cout << "Enter  file " << i << " path: ";
 			while (inputs.back().size() == 0) {
 				cin >> path;
@@ -911,7 +1078,7 @@ int main() {
 							line.erase(pos, 1);
 						if ((pos = line.find("//")) != std::string::npos)
 							line.erase(pos, line.size() - pos);
-						inputs.back() += line;
+						inputs.back().push_back(line);
 					}
 
 					myfile.close();
@@ -930,22 +1097,35 @@ int main() {
 
 		stack.push_back("$");
 		for (auto& input : inputs) {
-			input += "$";
+			input.push_back("$");
 			stack.push_back(gramm[0][0][0]);
 			Node* root = init(gramm[0][0][0]);
+
+			root->program_point[0] = 1;
+			root->program_point[1] = 1;
 			Node* current = root;
 			roots.push_back(root);
 			outputs.push_back(vector<string>());
+			int point1 = current->program_point[0];
+			int point2 = current->program_point[1];
 
 			while (1) {
 				if (stack.back().size() == 1 || stack.back().front() != '<' && stack.back().back() != '>') {
-					if (input.find(stack.back()) == 0) {
-						if (input == "$") {
+					if (input.front().find(stack.back()) == 0) {
+						if (input.front() == "$") {
 							fout << "String accepted" << endl;;
 							break;
 						}
 						outputs.back().push_back(stack.back());
-						input.erase(0, stack.back().size());
+						input.front().erase(0, stack.back().size());
+						current->program_point[0] = point1;
+						current->program_point[1] = point2;
+						point2++;
+						if (input.front().size() == 0) {
+							input.erase(input.begin());
+							point1++;
+							point2 = 1;
+						}
 						while (current->next == 0 && current->parent != 0) {
 							current = current->parent;
 						}
@@ -962,11 +1142,12 @@ int main() {
 					ind = find_vector(first, stack.back());
 					if (ind != -1) {
 						for (int i = 1; i < first[ind].size(); i++) {
-							if (input.find(first[ind][i].first) == 0) {
-								stack.pop_back();
+							if (input.front().find(first[ind][i].first) == 0) {
+								stack.pop_back();								
 								for (int j = first[ind][i].second.size() - 1; j >= 0; j--) {
 									Node* new_child = init(first[ind][i].second[j]);
 									new_child->parent = current;
+									new_child->program_point[0] = point1;
 									if (!current->children.empty())
 										new_child->next = current->children.front();
 									else
@@ -979,7 +1160,7 @@ int main() {
 							}
 							if (i == first[ind].size() - 1) {
 								int ind_2 = find_vector(follow, stack.back());
-								if (find(follow[ind_2], string(1, input.front())) != -1) {
+								if (find(follow[ind_2], string(1, input.front().front())) != -1) {
 									ind_1 = find(first[ind], "empty");
 									if (ind_1 != -1) {
 										stack.pop_back();
@@ -1006,6 +1187,7 @@ int main() {
 											for (int z = first[ind][j].second.size() - 1; z >= 0; z--) {
 												Node* new_child = init(first[ind][j].second[z]);
 												new_child->parent = current;
+												new_child->program_point[0] = point1;
 												if (!current->children.empty())
 													new_child->next = current->children.front();
 												else
@@ -1032,13 +1214,20 @@ int main() {
 							string name_buff = current->name;
 							name_buff.erase(0, 1);
 							name_buff.erase(name_buff.size() - 1, 1);
-							while (find(follow[ind], string(1, input.front())) == -1) {
-								input.erase(input.begin());
+							for (int j = 0; j < follow[ind].size(); j++) {
+								if (input.front().find(follow[ind][j].first) == 0)
+									break;
+								if (j == follow[ind].size() - 1) {
+									input.front().erase(input.front().begin());
+									j = 0;
+								}								
 							}
-						
 						Node* new_child = init(name_buff);
 						new_child->parent = current;
 						new_child->next = 0;
+						new_child->program_point[0] = point1;
+						new_child->program_point[1] = point2;
+						point2++;
 						current->children.push_back(new_child);
 						while (current->next == 0 && current->parent != 0) {
 							current = current->parent;
@@ -1053,32 +1242,59 @@ int main() {
 		}
 	
 		vector<Node*> roots_ast;
+		int lab = 1;
 		for (int i = 0; i <= 1; i++) {
 			Node* root_ast = init(roots[i]->name);
+			root_ast->label = lab;
 			roots_ast.push_back(root_ast);
 			Node* current_ast = root_ast;
 			Node *current = roots[i];
+			lab++;
 
 			while (1) {
 				if (!current->children.empty()) {
 					for (auto& single : current->children) {
 						if (single->children.empty()) {
+							if (current->children.size() != 1) {
+								Node* block = init("<block"+to_string(current->children.size())+">");
+								block->parent = current_ast;
+								block->label = lab;
+								lab++; 
+								if (current_ast->children.size() != 0)
+									current_ast->children.back()->next = block;
+								current_ast->children.push_back(block);
+								current_ast = block;
+							}
 							vector<Node*> child_buff;
 							for (int z = current->children.size() - 1; z >= 0; z--) {
-								Node* new_child = init(current->children[z]->name);
+								Node* new_child = new Node();
+								if (current->children.size() == 1 && current->children[z]->name.front() != '<' && current->children[z]->name.size()!=1 && current->children[z]->name != "empty") {
+									string name = current->name;
+									name.erase(0, 1);
+									name.erase(name.size()-1, 1);
+									new_child = init(name);
+								}
+								else
+									new_child = init(current->children[z]->name);
 								new_child->parent = current_ast;
 								if (!child_buff.empty())
 									new_child->next = child_buff.front();
 								else
 									new_child->next = 0;
+								new_child->label = lab + z;
+								new_child->program_point[0] = current->children[z]->program_point[0];
+								new_child->program_point[1] = current->children[z]->program_point[1];
 								child_buff.insert(child_buff.begin(), new_child);
 							}
+							lab = lab+child_buff.size();
 							if (current_ast->children.empty())
 								current_ast->children.insert(current_ast->children.begin(), child_buff.begin(), child_buff.end());
 							else {
 								current_ast->children.back()->next = child_buff.front();
 								current_ast->children.insert(current_ast->children.end(), child_buff.begin(), child_buff.end());
 							}
+							if (current_ast->name.find("<block") != string::npos)
+								current_ast->program_point[0] = current_ast->children.front()->program_point[0];
 							current_ast = child_buff.front();
 							break;
 
@@ -1094,17 +1310,25 @@ int main() {
 				else {
 					if (current->next != 0) {
 						current = current->next;
-						current_ast = current_ast->next;
+						if (current_ast->next == 0)
+							current_ast = current_ast->parent;
+						else
+							current_ast = current_ast->next;
 					}
 					else {
-						current_ast = current_ast->parent;
+						if(current_ast->parent!=0)
+							current_ast = current_ast->parent;
+						if(current_ast->name.find("<block")!=string::npos)
+							current_ast = current_ast->parent;
 						while (current->next == 0 && current->parent != 0) {
+							if(current_ast->parent!=0 && current_ast->parent->name.find("<block") != string::npos && current->parent->name == current_ast->parent->parent->name)
+								current_ast = current_ast->parent;
 							if (current_ast->parent != 0 && current->parent->name == current_ast->parent->name)
-								if(current_ast->name!= current_ast->parent->name)
+								if (current_ast->name != current_ast->parent->name) 
 									current_ast = current_ast->parent;
 							current = current->parent;
 						}
-						if (current->parent == 0 || current_ast->parent == 0)
+						if (current->parent == 0 && current_ast->parent == 0)
 							break;
 						if (current->name == current_ast->name)
 							current_ast = current_ast->next;
@@ -1116,7 +1340,58 @@ int main() {
 			}
 
 		}
+
 		
+		for (int i = 0; i <= 1; i++) {
+			Node* current = roots_ast[i];
+			while (1) {				
+				for (int j = 0; j < current->children.size(); j++) {
+					if (current->children[j]->name == "empty") {
+						current->children.erase(current->children.begin() + +j);
+						j--;
+
+						if (j < current->children.size() - 1 && j != -1)
+							current->children[j]->next = current->children[j + 1];
+						else {
+							if (j == current->children.size() - 1 && j!=-1)
+								current->children[j]->next = 0;
+						}
+					}			
+				}
+				if (!current->children.empty()) {
+					current = current->children.front();
+				}
+					
+				else {
+					bool flag = false;
+					while (current->next == 0) {
+						if (current != current->parent->children.back()) {
+							for (int z = 0; z < current->parent->children.size(); z++) {
+								if (current->parent->children[z] == current) {
+									current = current->parent->children[z + 1];
+									flag = true;
+									break;
+								}
+							}
+						}
+						else {
+							current = current->parent;
+						}	
+						if (current->parent == 0)
+							break;
+					}
+					if (current->parent == 0)
+						break;
+					else {
+						if (flag == false)
+							current = current->next;
+						else
+							flag = false;
+					}
+						
+				}
+			}
+		}
 
 		for (int i = 0; i <= 1; i++) {
 			fout << i+1 <<"tree:" << endl;
@@ -1128,10 +1403,81 @@ int main() {
 				fout << endl;
 			}
 		}
+
+
+		map<string, set<Pattern*>> patterns;
+		set<string> worktable;
+		set<string>::iterator it;
+		Node* current1 = roots_ast.front();
+		Node* current2 = roots_ast.back();
+		while (1) {
+			it = worktable.find( to_string(current1->label) + ";" + to_string(current2->label) + ";" );
+			if (it == worktable.end()) {
+				worktable.insert( to_string(current1->label) + ";" + to_string(current2->label) + ";" );
+				if (current1->name == current2->name) {
+					Pattern* buff_pattern = anty_unity(current1, current2, worktable);
+					map<string, set<Pattern*>>::iterator it1 = patterns.find(current1->name);
+					if (it1 == patterns.end()) 
+						patterns.insert(pair<string, set<Pattern*>>(current1->name, set<Pattern*>()));
+					patterns[current1->name].insert(buff_pattern);
+				}
+			}
+			if (current2->next != 0)
+				current2 = current2->next;
+			else {
+				if (current1->next != 0) {
+					current1 = current1->next;
+					current2 = current2->parent->children.front();
+				}
+				else {
+					if (current1->parent == 0 && current2->parent == 0) {
+						current1 = current1->children.front();
+						current2 = current2->children.front();
+					}
+					else {
+						current1 = current1->parent->children.front();
+						current2 = current2->parent->children.front();
+						while (1) {
+							if (current1->children.size() != 0 && current1->children.size() == current2->children.size()) {
+								current1 = current1->children.front();
+								current2 = current2->children.front();
+								break;
+							}
+							else {
+								while (current1->next == 0 && current1->parent != 0 && current2->next == 0 && current2->parent != 0) {
+									current1 = current1->parent;
+									current2 = current2->parent;
+								}
+								if (current1->parent != 0 && current2->parent != 0) {
+									current1 = current1->next;
+									current2 = current2->next;
+								}
+								else
+									break;
+							}
+						}
+						if (current1->parent == 0 && current2->parent == 0)
+							break;
 		
-		
+					}
+
+				}
+			}
+
+		}
+
+
+		for (auto& elem : patterns) {
+			if (elem.first == "no") {
+				fout << "Plagiat not detected";
+				fout.close();
+				return 0;
+			}
+		}
 
 		
+		fout << "Plagiat detected";
+		fout.close();
 		return 0;
 	}
 }
